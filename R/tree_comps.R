277ee607dc15f958d4cf96425c9a78e63355ebb0
Jacob Englert <57627602+jacobenglert@users.noreply.github.com>
2023-06-21 08:13:04 -0400

    remove 'survival' prefix in tree_comps R script

--- R/tree_comps.R
+++ R/tree_comps.R
@@ -57,7 +57,7 @@ comp_mv <- function(m_start = 0, y, z, stratum, sc1 = 0, lambda = 0, sigma2_mu, 
     grd <- comp_grd(y, z, sc1 + z * (m + lambda), stratum, max_win, na_locs) - (m / sigma2_mu)
     I <- comp_fisher(y, z, sc1 + z * (m + lambda), stratum, max_win, na_locs)  + (1 / sigma2_mu)
     if(i == 10 | is.nan(I) | is.nan(grd)){
-      m <- stats::coef(survival::clogit(y ~ offset(sc1) + offset(z * lambda) + z + survival::strata(stratum)))
+      m <- stats::coef(clogit(y ~ offset(sc1) + offset(z * lambda) + z + strata(stratum)))
       grd <- comp_grd(y, z, sc1 + z * (m + lambda), stratum, max_win, na_locs) - (m / sigma2_mu)
       I <- comp_fisher(y, z, sc1 + z * (m + lambda), stratum, max_win, na_locs)  + (1 / sigma2_mu)
     }
